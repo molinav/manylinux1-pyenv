5cd17d6cf005313f81fa7f91e94299f0913def20
Víctor Molina García <molinav@users.noreply.github.com>
2020-12-22 19:59:09 +0100

    Update logging prints

--- scripts/perl_install.sh
+++ scripts/perl_install.sh
@@ -5,19 +5,19 @@ set -e
 perl_version=5.30.2
 perl_prefix=/opt/perl
 
-echo "      ---> Downloading Perl..."
+echo "      ---> Perl5: downloading..."
 curl -s https://www.cpan.org/src/5.0/perl-${perl_version}.tar.gz -O
 tar -xzf perl-${perl_version}.tar.gz
 
 cd perl-${perl_version}
-echo "      ---> Configuring Perl..."
+echo "      ---> Perl5: configuring..."
 sh Configure -des -Dprefix=${perl_prefix} >/dev/null
-echo "      ---> Building Perl..."
+echo "      ---> Perl5: building..."
 make -j8 >/dev/null 2>&1
-echo "      ---> Installing Perl..."
+echo "      ---> Perl5: installing..."
 make install >/dev/null 2>&1
 
-echo "      ---> Cleaning..."
+echo "      ---> Perl5: cleaning..."
 cd ..
 rm -rf perl-${perl_version}
 rm -f perl-${perl_version}.tar.gz
--- scripts/ssl10_install.sh
+++ scripts/ssl10_install.sh
@@ -9,31 +9,31 @@ openssl_name=openssl-${openssl_version}
 openssl_targz=$openssl_name.tar.gz
 openssl_patch=openssl-$openssl_version-fix_parallel_build-1.patch
 
-echo "      ---> Downloading OpenSSL ${openssl_version}..."
+echo "      ---> OpenSSL ${openssl_version}: downloading..."
 curl -s https://www.openssl.org/source/openssl-${openssl_version}.tar.gz -O
 curl -s http://www.linuxfromscratch.org/patches/blfs/7.7/$openssl_patch -O
 tar -xzf openssl-${openssl_version}.tar.gz
 
 PATH=/opt/perl/bin:$PATH
 cd openssl-${openssl_version}
-echo "      ---> Patching OpenSSL ${openssl_version}..."
-patch -Np1 -i ../$openssl_patch
-echo "      ---> Configuring OpenSSL ${openssl_version}..."
+echo "      ---> OpenSSL ${openssl_version}: patching..."
+patch -Np1 -i ../$openssl_patch >/dev/null
+echo "      ---> OpenSSL ${openssl_version}: configuring..."
 ./config --prefix=${openssl_prefix} --openssldir=${openssl_prefix}/ssl        \
          --libdir=lib -Wl,-rpath=${openssl_prefix}/lib                        \
-         shared zlib-dynamic
-echo "      ---> Building OpenSSL ${openssl_version}..."
-make -j8
-echo "      ---> Installing OpenSSL  ${openssl_version}..."
-make install_sw
+         shared zlib-dynamic >/dev/null
+echo "      ---> OpenSSL ${openssl_version}: building..."
+make >/dev/null 2>&1
+echo "      ---> OpenSSL ${openssl_version}: installing..."
+make install >/dev/null 2>&1
 
 cd ..
-echo "      ---> Cleaning..."
+echo "      ---> OpenSSL ${openssl_version}: cleaning..."
 rm -rf $openssl_name
 rm -f $openssl_targz
 rm -f $openssl_patch
 
-echo "Linking CA certificates..."
+echo "      ---> OpenSSL ${openssl_version}: linking CA certificates..."
 rmdir ${openssl_prefix}/ssl/certs
 ln -s /etc/pki/tls/certs ${openssl_prefix}/ssl/certs
 ln -s /etc/pki/tls/cert.pem ${openssl_prefix}/ssl/cert.pem
