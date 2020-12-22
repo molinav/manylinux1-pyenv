2c9392d7f21da3261c5bc183fca7d358a83c7250
Víctor Molina García <molinav@users.noreply.github.com>
2020-12-22 23:13:27 +0100

    Add pip/setuptools/wheel update to Python script

--- scripts/python_install.sh
+++ scripts/python_install.sh
@@ -15,3 +15,32 @@ export LDFLAGS="-L${openssl_prefix}/lib"
 eval "$(pyenv init -)"
 pyenv install "$python_xyz"
 ln -s $PYENV_ROOT/versions/${python_xyz} $PYENV_ROOT/versions/${python_version}
+
+# Upgrade pip, setuptools and wheel.
+pyenv shell ${python_version}
+if [ "$python_version" = "2.6" ]; then
+    pip install --no-cache-dir --upgrade "pip < 10"
+    pip install --no-cache-dir --upgrade "wheel < 0.30"
+    pip install --no-cache-dir --upgrade "setuptools < 37"
+elif [ "$python_version" = "2.7" ]; then
+    pip install --no-cache-dir --upgrade "pip < 21"
+    pip install --no-cache-dir --upgrade "wheel < 0.36"
+    pip install --no-cache-dir --upgrade "setuptools < 45"
+elif [ "$python_version" = "3.2" ]; then
+    pip install --no-cache-dir --upgrade "pip < 7.1.1"
+    pip install --no-cache-dir --upgrade "wheel < 0.32"
+    pip install --no-cache-dir --upgrade "setuptools < 30"
+elif [ "$python_version" = "3.3" ]; then
+    pip install --no-cache-dir --upgrade "pip < 18"
+    pip install --no-cache-dir --upgrade "wheel < 0.30"
+    pip install --no-cache-dir --upgrade "setuptools < 40"
+elif [ "$python_version" = "3.4" ]; then
+    pip install --no-cache-dir --upgrade "pip < 20"
+    pip install --no-cache-dir --upgrade "wheel < 0.34"
+    pip install --no-cache-dir --upgrade "setuptools < 44"
+else
+    pip install --no-cache-dir --upgrade "pip < 21"
+    pip install --no-cache-dir --upgrade "wheel < 0.36"
+    pip install --no-cache-dir --upgrade "setuptools < 50"
+fi
+pyenv shell system
